9be4cde3501ec8d3e0509b31a68caeff153d52b0
JaloliddinXabibjonov <88301971+JaloliddinXabibjonov@users.noreply.github.com>
2022-02-01 11:50:47 +0500

    Update AttachmentController.java

--- src/main/java/uz/pdp/warehouse/controller/AttachmentController.java
+++ src/main/java/uz/pdp/warehouse/controller/AttachmentController.java
@@ -24,7 +24,7 @@ public class AttachmentController {
     }
 
                         // GET ALL FILES
-    @GetMapping("/getAll)
+    @GetMapping("/getAll")
     public List<Attachment> getAllFiles(){
         return attachmentService.getAllFilesService();
     }
